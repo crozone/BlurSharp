e73cedbe64d9365485a4d6c9bc951878fd1ab3df
Ryan Crosby <ryan.crosby@live.com>
2020-02-23 02:22:38 +1100

    Update tests to use the doughnut image and hashes from the python BlurHash project

--- src/BlurSharp.Tests/BursharpDrawingTests.cs
+++ src/BlurSharp.Tests/BursharpDrawingTests.cs
@@ -7,14 +7,47 @@ namespace BlurSharp.Tests
 {
     public class BursharpDrawingTests
     {
+        private static readonly string Doughnut43Hash = "LlMF%n00%#MwS|WCWEM{R*bbWBbH";
+        private static readonly string DoughnutBlackAndWhite43Hash = "LjIY5?00?bIUofWBWBM{WBofWBj[";
+
         [Fact]
-        public void Test1()
+        public void Doughnut43FullTest()
         {
-            Bitmap bitmap = new Bitmap(Image.FromFile("./img/wave.bmp"));
+            Bitmap bitmap = new Bitmap(Image.FromFile("./img/doughnut.png"));
+            string hashResult = BlurHashDrawing.Encode(bitmap, 4, 3);
+            Assert.Equal(Doughnut43Hash, hashResult);
+        }
 
-            string hashResult = BlurHashDrawing.Encode(bitmap, 3, 3);
+        [Fact]
+        public void Doughnut43NumComponentsTest()
+        {
+            Bitmap bitmap = new Bitmap(Image.FromFile("./img/doughnut.png"));
+            string hashResult = BlurHashDrawing.Encode(bitmap, 4, 3);
+            Assert.Equal(Doughnut43Hash.Substring(0, 1), hashResult.Substring(0, 1));
+        }
 
-            Assert.Equal("KDFP~q_NtS00%1NHELIBMc", hashResult);
+        [Fact]
+        public void Doughnut43MaxACTest()
+        {
+            Bitmap bitmap = new Bitmap(Image.FromFile("./img/doughnut.png"));
+            string hashResult = BlurHashDrawing.Encode(bitmap, 4, 3);
+            Assert.Equal(Doughnut43Hash.Substring(1, 1), hashResult.Substring(1, 1));
+        }
+
+        [Fact]
+        public void Doughnut43AverageColorTest()
+        {
+            Bitmap bitmap = new Bitmap(Image.FromFile("./img/doughnut.png"));
+            string hashResult = BlurHashDrawing.Encode(bitmap, 4, 3);
+            Assert.Equal(Doughnut43Hash.Substring(2, 4), hashResult.Substring(2, 4));
+        }
+
+        [Fact]
+        public void DoughnutBlackAndWhite43FullTest()
+        {
+            Bitmap bitmap = new Bitmap(Image.FromFile("./img/doughnut_black_and_white.png"));
+            string hashResult = BlurHashDrawing.Encode(bitmap, 4, 3);
+            Assert.Equal(DoughnutBlackAndWhite43Hash, hashResult);
         }
     }
 }
