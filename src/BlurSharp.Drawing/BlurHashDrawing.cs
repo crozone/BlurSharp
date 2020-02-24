16e4fa48c80661b4da64c6f2ff2745d741560d40
Ryan Crosby <ryan.crosby@live.com>
2020-02-24 12:47:11 +1100

    Cleanup drawing wrapper

--- src/BlurSharp.Drawing/BlurHashDrawing.cs
+++ src/BlurSharp.Drawing/BlurHashDrawing.cs
@@ -9,12 +9,12 @@ namespace BlurSharp.Drawing
         // TODO: Create another version of this using an open source, portable image library.
         public static string Encode(Bitmap bufferedImage, int componentX, int componentY)
         {
-            int width = bufferedImage.Width;
-            int height = bufferedImage.Height;
-
             BitmapData curBitmapData = bufferedImage.LockBits(
-                new Rectangle(0, 0, width, height),
+                new Rectangle(0, 0, bufferedImage.Width, bufferedImage.Height),
                 ImageLockMode.ReadOnly, PixelFormat.Format24bppRgb);
+
+            int height = curBitmapData.Height;
+            int width = curBitmapData.Width;
 
             // The stride is the width of a single row of pixels (a scan line), rounded up to a four-byte boundary.
             // If the stride is positive, the bitmap is top-down. If the stride is negative, the bitmap is bottom-up.
@@ -22,12 +22,11 @@ namespace BlurSharp.Drawing
             int stride = curBitmapData.Stride;
             int dataLength = Math.Abs(stride) * height;
 
-            ReadOnlySpan<byte> bitmapData;
-
             // We need an unsafe context to get a Span reference to the bitmap data without a copy.
-            // We beed to make sure that we get the length correct, and that UnlockBits is not called
+            // We must make sure to get the length correct, and that UnlockBits is not called
             // before we are finished with the bitmapData Span.
             //
+            ReadOnlySpan<byte> bitmapData;
             unsafe
             {
                 bitmapData = new ReadOnlySpan<byte>(curBitmapData.Scan0.ToPointer(), dataLength);
